77fc10397fd8bf113a64543f906025ba37440d4c
hanuuuuU <han980004@naver.com>
2025-10-02 13:52:09 +0900

    feat(main): 메인 레이아웃 틀 잡기

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -3,15 +3,5 @@ export default function AuthLayout({
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
-	return (
-		<div
-			className="min-h-screen bg-white"
-			style={{
-				width: 'var(--mobile-container-width)',
-				margin: '20px 22px 0 22px',
-			}}
-		>
-			{children}
-		</div>
-	);
+	return <div className="h-full w-full bg-white">{children}</div>;
 }
