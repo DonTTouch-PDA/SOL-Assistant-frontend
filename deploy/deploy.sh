855bc6181023a34f3375848ca86c9574d904ca77
JeongHyeon Jo <cisxo@naver.com>
2025-10-10 05:46:00 +0900

    test(CodeDeploy): CodeDeploy test1

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,11 +8,15 @@ APP_DIR="/home/ec2-user/$APP_NAME"
 docker stop $APP_NAME || true
 docker rm $APP_NAME || true
 
-# 2. 새 이미지 로드 (zip 안에 tar 포함된 경우)
-docker load -i $APP_DIR/$APP_NAME.tar
+# 2. 최신 tar 파일 찾기
+TAR_FILE=$(ls $APP_DIR/sol-assistant-*.tar | sort | tail -n 1)
+echo "Loading Docker image from $TAR_FILE"
 
-# 3. 새 컨테이너 실행
+# 3. 새 이미지 로드
+docker load -i $TAR_FILE
+
+# 4. 새 컨테이너 실행
 docker run -d -p 3000:3000 --name $APP_NAME $APP_NAME
 
-# 4. 실행 상태 확인
+# 5. 실행 상태 확인
 docker ps | grep $APP_NAME
