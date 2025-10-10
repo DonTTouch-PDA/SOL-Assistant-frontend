206279dd853bac22cfaf66d710fcd0e70e90a83e
JeongHyeon Jo <cisxo@naver.com>
2025-10-10 16:38:34 +0900

    test(CodeDeploy): zero time deployment comple

--- deploy/health_check.sh
+++ deploy/health_check.sh
@@ -5,7 +5,7 @@ TARGET_DIR="/home/ec2-user/includes"
 TARGET_PORT=$(cat $TARGET_DIR/target_port.inc)
 
 for i in {1..10}; do
-  RESPONSE=$(curl -s http://127.0.0.1:${TARGET_PORT}/health || true)
+  RESPONSE=$(curl -s http://127.0.0.1:${TARGET_PORT}/api/health || true)
   if [ "$RESPONSE" = "ok" ]; then
     echo "> Health check passed!"
     exit 0
