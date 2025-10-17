ff8f1c7419e9f5c8458cb6582a118a62bcb684ae
곽예경 <kyun9.k@gmail.com>
2025-10-17 14:21:09 +0900

    유사차트#32

--- src/containers/similar/SimilarDetailContainer.tsx
+++ src/containers/similar/SimilarDetailContainer.tsx
@@ -6,6 +6,8 @@ import { SIMILAR_DETAIL_BOLLINGER_BANDS } from '@/constants/descriptions';
 import { SimilarChartDetail } from '@/types/similar';
 import { useRouter } from 'next/navigation';
 import SimilarChart from '@/components/similar/SimilarChart';
+import CustomCard from '@/components/common/CustomCard';
+import CustomPopOver from '@/components/common/customPopover';
 
 const dummyData = {
 	stockName: 'naver',
@@ -261,6 +263,22 @@ export default function SimilarDetailContainer({
 		getSimilarChart();
 	}, []);
 
+	const [showPopover, setShowPopover] = useState(false);
+
+	useEffect(() => {
+		const showTimer = setTimeout(() => {
+			setShowPopover(true);
+
+			const hideTimer = setTimeout(() => {
+				setShowPopover(false);
+			}, 2000);
+
+			return () => clearTimeout(hideTimer);
+		}, 500);
+
+		return () => clearTimeout(showTimer);
+	}, []);
+
 	return (
 		<motion.div
 			className="absolute inset-0 z-50 bg-white flex flex-col mt-11"
@@ -269,41 +287,63 @@ export default function SimilarDetailContainer({
 			exit={{ x: '100%' }}
 			transition={{ type: 'tween', duration: 0.3 }}
 		>
-			{/* 헤더 */}
+			{/* 상단 */}
 			<div className="flex items-center -ml-[10px]">
 				<ChevronLeft size={30} onClick={onclose} />
 				<p>과거 유사 차트</p>
 			</div>
-			<div className="">
-				{/* body */}
-				<div className="flex justify-between items-center border-b border-gray-200 py-4 -mx-5 px-5 bg-white">
-					<p className="text-[30px] pl-[10px] font-bold">{stockName}</p>
-					<button
-						className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm"
-						onClick={() => {
-							router.push(`/${stockCode}`);
-						}}
-					>
-						주문
-					</button>
+			{/* 헤더 */}
+			<div className="flex justify-between items-center border-b border-gray-200 py-4 -mx-5 px-5 bg-white">
+				<p className="text-[30px] pl-[10px] font-bold">{stockName}</p>
+				<button
+					className="bg-blue-600 text-white px-4 py-2 rounded-lg text-sm"
+					onClick={() => {
+						router.push(`/${stockCode}`);
+					}}
+				>
+					주문
+				</button>
+			</div>
+			<div className="flex flex-col items-center px-2 gap-2">
+				<div className="flex items-center gap-1">
+					<CircleAlert size={16} />
+					<p className="text-[12px] py-[12px]">
+						오늘과 유사한 매매 신호가 나타났던 과거 {stockName}의 차트예요.
+					</p>
 				</div>
-				<div className="flex flex-col items-center">
-					<div className="flex items-center gap-1">
-						<CircleAlert size={16} />
-						<p className="text-[12px] py-[12px]">
-							오늘과 유사한 매매 신호가 나타났던 과거 {stockName}의 차트예요.
+				<div
+					className="w-full mb-[16px] h-[250px]"
+					onClick={() => {
+						setShowPopover(true);
+
+						const hideTimer = setTimeout(() => {
+							setShowPopover(false);
+						}, 2000);
+
+						return () => clearTimeout(hideTimer);
+					}}
+				>
+					<SimilarChart data={dummyData} />
+				</div>
+				<div className="relative">
+					<div className="absolute top-0 right-0 translate-x-[10px] -translate-y-[15px]">
+						<CustomPopOver
+							text="이날 볼린저밴드 하단선을 터치했어요!"
+							upSideDown={true}
+							isShowPopover={showPopover}
+						/>
+					</div>
+				</div>
+				<CustomCard>
+					<div>
+						<p className="text-[20px] font-bold pb-1">
+							{similarChart?.signalType}
 						</p>
-					</div>
-					<div className="w-full mb-[16px] h-[250px] pt-[16px] ">
-						<SimilarChart data={dummyData} today={new Date()} />
-					</div>
-					<div className="w-full  border-gray-100 border shadow-md p-4 rounded-xl">
-						<p className="text-[20px] font-bold">{similarChart?.signalType}</p>
 						<p className="text-[16px] leading-5">
 							{SIMILAR_DETAIL_BOLLINGER_BANDS}
 						</p>
 					</div>
-				</div>
+				</CustomCard>
 			</div>
 		</motion.div>
 	);
